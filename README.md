03246e5c5f7f8cd5dccde44b8ee19c570241cec3
diegodls <dark.dls.lol@gmail.com>
2020-05-06 15:51:26 -0300

    Criando assets para o README

--- README.md
+++
@@ -1,2 +0,0 @@
-# orientation_react_native
-Projeto simples para demonstrar o uso e manipulação de orientação do dispositivo no react native
--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
-import Navigator from './src/navigation/Navigator'
-
-AppRegistry.registerComponent(appName, () => Navigator);
